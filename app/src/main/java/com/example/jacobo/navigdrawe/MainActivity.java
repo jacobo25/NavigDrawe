0231aac4cc2c826543f4b3efec3ff81c02d9b669
jacobo25 <jacobo.ldo@hotmail.com>
2017-04-13 22:09:31 -0500

    Version 4

--- app/src/main/java/com/example/jacobo/navigdrawe/MainActivity.java
+++ app/src/main/java/com/example/jacobo/navigdrawe/MainActivity.java
@@ -1,5 +1,126 @@
 package com.example.jacobo.navigdrawe;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.jacobo.navigdrawe.R;
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        agregarToolbar();
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        if (navigationView != null) {
+            prepararDrawer(navigationView);
+            // Seleccionar item por defecto
+            seleccionarItem(navigationView.getMenu().getItem(0));
+        }
+    }
+
+    private void agregarToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            // Poner Ã­cono del drawer toggle
+            ab.setHomeAsUpIndicator(R.drawable.ic_home_black_18dp);
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
+
+    }
+
+    private void prepararDrawer(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        seleccionarItem(menuItem);
+                        drawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+
+    }
+
+    private void seleccionarItem(MenuItem itemDrawer) {
+        Fragment fragmentoGenerico = null;
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        switch (itemDrawer.getItemId()) {
+            case R.id.nav_camera:
+                fragmentoGenerico = new AzulFragment();
+                break;
+            case R.id.nav_gallery:
+                fragmentoGenerico = new VerdeFragment();
+                break;
+            case R.id.nav_slideshow:
+                fragmentoGenerico = new AmarilloFragment();
+                break;
+            case R.id.nav_manage:
+                fragmentoGenerico = new RedFragment();
+                break;
+            case R.id.nav_share:
+                startActivity(new Intent(this, HotelActivity.class));
+                break;
+            case R.id.nav_send:
+                fragmentoGenerico = new HotelesFragment();
+                break;
+        }
+        if (fragmentoGenerico != null) {
+            fragmentManager
+                    .beginTransaction()
+                    .replace(R.id.Contenedor, fragmentoGenerico)
+                    .commit();
+        }
+
+        // Setear tÃ­tulo actual
+        setTitle(itemDrawer.getTitle());
+
+
+
+    }
+
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_drawer_drawer, menu);
+        return true;
+    }*/
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
+
+/*
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -16,7 +137,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, AzulFragment.OnFragmentInteractionListener, VerdeFragment.OnFragmentInteractionListener,RedFragment.OnFragmentInteractionListener {
+        implements NavigationView.OnNavigationItemSelectedListener, AzulFragment.OnFragmentInteractionListener,
+        VerdeFragment.OnFragmentInteractionListener, RedFragment.OnFragmentInteractionListener,
+        AmarilloFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -93,14 +216,20 @@ public class MainActivity extends AppCompatActivity
             fragment = new VerdeFragment();
             FragmentoSeleciionado = true;
         } else if (id == R.id.nav_slideshow) {
-            fragment = new RedFragment();
+            fragment = new AmarilloFragment();
             FragmentoSeleciionado = true;
 
         } else if (id == R.id.nav_manage) {
+            fragment = new RedFragment();
+            FragmentoSeleciionado = true;
 
         } else if (id == R.id.nav_share) {
+            Intent intent = new Intent(MainActivity.this,HotelActivity.class);
+            startActivity(intent);
 
         } else if (id == R.id.nav_send) {
+            fragment = new HotelesFragment();
+            FragmentoSeleciionado = true;
 
         }
         if (FragmentoSeleciionado){
@@ -116,3 +245,4 @@ public class MainActivity extends AppCompatActivity
 
     }
 }
+*/
